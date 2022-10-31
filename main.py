925af77b4830a52b08c34b55b657fc7a069bebdf
Daniel Arribas <70711844+daniarribas@users.noreply.github.com>
2022-10-31 15:57:29 +0100

    code  as taken from the original version of the project

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-import pandas as pd
-import matplotlib as pd
+from bs4 import BeautifulSoup
+import requests
+from csv import writer
+pag =1
+pages = 66
+with open('pararius.csv', 'w', encoding='utf8', newline='') as f:
+  thewriter = writer(f)
+  header = ['Title','Location','Price','Area']
+  thewriter.writerow(header)
+  while pag<=pages :
+    url = "https://www.pararius.com/apartments/nederland/page-" + str(pag)
+    page = requests.get(url)
+    
+    soup = BeautifulSoup(page.content, 'html.parser')
+    
+    lists =  soup.find_all('section',class_="listing-search-item")
+    for list in lists:
+     title = list.find('a', class_="listing-search-item__link--title").text.replace('\n','')
+     location = list.find('div', class_="listing-search-item__sub-title").text.replace('\n','')
+     price = list.find('div', class_="listing-search-item__price").text.replace('\n','')
+     area = list.find('li', class_="illustrated-features__item--surface-area").text.replace('\n','')
+     info = [title, location, price, area]
+     thewriter = writer(f)
+     thewriter.writerow(info)
+      
+    pag += 1
+print("finished")
